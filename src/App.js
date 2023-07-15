5a0490e13bd43e1c9071523aabd6e065cb46b704
Zachary <zhs001@aquinas.edu>
2023-07-14 20:58:40 -0400

    React Todo App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todo, setTodo] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  function addTodo(){
+    const items = {
+      id: Math.floor(Math.random() * 1000),
+      name: newTodo
+    };
+
+    setTodo(oldTodo => [...oldTodo, items]);
+    setNewTodo('');
+  }
+
+  function deleteTodo(id){
+    const deletedArray = todo.filter(item => item.id !== id);
+    setTodo(deletedArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='centered'>
+        <label htmlFor='todoInput' id='todoLabel'>Enter your todo</label>
+        <input 
+        type='text' 
+        value={newTodo} 
+        id='todoInput' 
+        placeholder='I have to code' 
+        onChange={e => setNewTodo(e.target.value)} />
+        <button onClick={addTodo}>Submit</button>
+        <ul>
+          {todo.map(item => {
+            return <li className='todoList' key={item.id}>{item.name} <button className='deleteBTN' onClick={() => deleteTodo(item.id)}>Delete</button></li>
+          })}
+        </ul>
+      </div>
     </div>
-  );
+  ) 
 }
 
 export default App;
